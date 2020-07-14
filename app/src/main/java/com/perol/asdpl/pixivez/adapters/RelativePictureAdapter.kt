315f1e2140ca0c7b470e78608a4cb5c813c8438f
ultranity <zjy1095429904@sina.com>
2020-07-14 19:36:18 +0800

    update: transparent background

--- app/src/main/java/com/perol/asdpl/pixivez/adapters/RelativePictureAdapter.kt
+++ app/src/main/java/com/perol/asdpl/pixivez/adapters/RelativePictureAdapter.kt
@@ -36,7 +36,7 @@ class RelativePictureAdapter(layoutResId: Int) : BaseQuickAdapter<String, BaseVi
     override fun convert(helper: BaseViewHolder, item: String) {
         val imageView = helper.getView<ImageView>(R.id.imageview_aboutpic)
         if (helper.layoutPosition % 2 != 0)
-            GlideApp.with(imageView.context).load(item).placeholder(R.color.white).transition(withCrossFade()).centerInside().into(imageView)
+            GlideApp.with(imageView.context).load(item).placeholder(R.color.transparent).transition(withCrossFade()).centerInside().into(imageView)
         else
             GlideApp.with(imageView.context).load(item).placeholder(R.color.gray).transition(withCrossFade()).centerInside().into(imageView)
     }

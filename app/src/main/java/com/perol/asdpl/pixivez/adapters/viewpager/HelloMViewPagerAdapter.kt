5671137e313f1607bef06e93781a4c45c83ff6f3
ultranity <zjy1095429904@sina.com>
2021-11-07 13:19:31 +0800

    adjust: tab swipe

---
+++ app/src/main/java/com/perol/asdpl/pixivez/adapters/viewpager/HelloMViewPagerAdapter.kt
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 ultranity
+ * Copyright (c) 2019 Perol_Notsfsssf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ */
+
+package com.perol.asdpl.pixivez.adapters.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.perol.asdpl.pixivez.fragments.hellom.HelloMDynamicsFragment
+import com.perol.asdpl.pixivez.fragments.hellom.HelloMThFragment
+import com.perol.asdpl.pixivez.fragments.hellom.HelloMainFragment
+
+class HelloMViewPagerAdapter(fragmentManager: FragmentManager) :
+    FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getCount(): Int = 3
+
+    private val fragments = Array<Fragment?>(3) {null}
+    override fun getItem(position: Int):Fragment {
+        if(fragments[position] == null)
+            fragments[position] = when (position) {
+                0 -> {
+                    HelloMainFragment.newInstance("s", "s")
+                }
+                1 -> {
+                    HelloMDynamicsFragment.newInstance("d")
+                }
+                2 -> {
+                    HelloMThFragment.newInstance("d", "c")
+                }
+                else -> {
+                    HelloMThFragment.newInstance("d", "c")
+                }
+            }
+        return fragments[position]!!
+    }
+}
